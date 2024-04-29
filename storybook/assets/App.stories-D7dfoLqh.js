be59c6fc68195ebbef38f1117b89b318e34f38ca
jinhokim98 <jinhokim98@users.noreply.github.com>
2024-04-29 13:23:29 +0000

    deploy: 84d0c6f9f5560d44b3464f875e1409e381a186c2

---
+++ storybook/assets/App.stories-D7dfoLqh.js
@@ -0,0 +1,18 @@
+import{u as i,j as n}from"./styled-components.browser.esm-DO55u7qc.js";import{r as o}from"./index-CsdIBAqE.js";/**
+ * React Router v6.23.0
+ *
+ * Copyright (c) Remix Software Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ *
+ * @license MIT
+ */const l=o.createContext({outlet:null,matches:[],isDataRoute:!1}),m=o.createContext(null);function p(e){let r=o.useContext(l).outlet;return r&&o.createElement(m.Provider,{value:e},r)}function d(e){return p(e.context)}new Promise(()=>{});const x=i.div`
+  position: relative;
+  width: 37.6rem;
+  min-height: 70rem;
+
+  background-color: ${e=>e.theme.color.white};
+  border: 0.1rem solid ${e=>e.theme.color.gray};
+  border-radius: 0.4rem;
+`;function c(){return n.jsx(x,{children:n.jsx(d,{})})}c.__docgenInfo={description:"",methods:[],displayName:"App"};const C={title:"App",component:c},t={};var s,a,u;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(u=(a=t.parameters)==null?void 0:a.docs)==null?void 0:u.source}}};const g=["Default"];export{t as Default,g as __namedExportsOrder,C as default};
---
+++ storybook/assets/Button.stories-BCoaoYJO.js
@@ -0,0 +1,30 @@
+import{u as m,j as l}from"./styled-components.browser.esm-DO55u7qc.js";import"./index-CsdIBAqE.js";const p=m.button`
+  width: 100%;
+  height: 100%;
+
+  border: none;
+  background-color: ${r=>r.theme.color.darkGray};
+  ${r=>r.theme.typography.button};
+  color: ${r=>r.theme.color.white};
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${r=>r.theme.color.black};
+  }
+
+  &:active {
+    background-color: ${r=>r.theme.color.black};
+  }
+`,u=({children:r,...i})=>l.jsx(p,{...i,children:r});u.__docgenInfo={description:"",methods:[],displayName:"Button"};const b={title:"Button",component:u},o={args:{children:"안녕"}},e={args:{children:"안녕",style:{borderRadius:8}}};var t,n,s;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: '안녕'
+  }
+}`,...(s=(n=o.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var a,c,d;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: '안녕',
+    style: {
+      borderRadius: 8
+    }
+  }
+}`,...(d=(c=e.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const k=["기본","테두리_둥글게"];export{k as __namedExportsOrder,b as default,o as 기본,e as 테두리_둥글게};
